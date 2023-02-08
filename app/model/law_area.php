51aeb7087d12716a0ba1adced766b3cb27c03b95
MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2023-02-08 12:45:42 +0100

    aa

--- app/model/law_area.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class law_area
-{
-    public int $type_id;
-    public string $description;
-
-}
--- app/public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$url = $_SERVER['REQUEST_URI'];
-session_start();
-require_once("../router/router.php");
-$router = new Router();
-$router->route($url);
