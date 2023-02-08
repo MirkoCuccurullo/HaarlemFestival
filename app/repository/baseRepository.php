48145fe755b4f8a06af78622ee5b29c4f94789d6
Mirko Cuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2023-02-08 17:28:00 +0000

    made router, and structure for mvc, added sql for the database. simple login/logout

---
+++ app/repository/baseRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace repository;
+
+use PDO;
+use PDOException;
+
+class baseRepository
+{
+    protected $connection;
+
+    public function __construct()
+    {
+        try {
+            require('../config.php');
+            $this->connection = new PDO("mysql:host=$db_host; dbname=$db_name", $db_username, $db_password);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
+    }
+}
