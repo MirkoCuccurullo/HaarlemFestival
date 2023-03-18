e9943731052361c3ac5c9a9ed180a27dec28f045
MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2023-03-18 20:58:16 +0100

    final update to QR Reader, now redirects automatically to the webpage.

--- app/view/festival/qr_checker.php
+++ app/view/festival/qr_checker.php
@@ -1,35 +1,37 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
+include __DIR__ . '/../header_dance.php';
+?>
 <script src="https://unpkg.com/html5-qrcode@2.0.9/dist/html5-qrcode.min.js"></script>
-<div id="qr-reader" style="width: 600px"></div>
-<div id="qr_link">
+<div style="flex-wrap: wrap; justify-content: center;">
+    <div id="qr-reader" class="m-3" style="width: 600px"></div>
+    <div id="qr_link" class="m-3">
 
+    </div>
 </div>
-<script>
-    var wasScanned = false;
-    function onScanSuccess(decodedText, decodedResult) {
-        var link_div = document.getElementById("qr_link");
-        var link = document.createElement("a");
-        link.href = decodedText;
-        link.innerHTML = "Click here";
-        link_div.appendChild(link);
-        wasScanned = true;
-    }
-    var html5QrcodeScanner = new Html5QrcodeScanner(
-        "qr-reader", { fps: 10, qrbox: 250 });
 
-    if (!wasScanned) {
-        html5QrcodeScanner.render(onScanSuccess);
+
+
+<?php
+include __DIR__ . '/../footer.php'; ?>
+<script>
+    changeFooterToDanceStyle();
+
+    function changeFooterToDanceStyle() {
+        document.getElementById("undernavbar").remove();
+        document.getElementById("footer").style.backgroundColor = "#d9d9d9";
     }
-    
+
+    function onScanSuccess(decodedText, decodedResult) {
+        console.log(decodedText);
+        createButtonLink(decodedText);
+    }
+
+    var html5QrcodeScanner = new Html5QrcodeScanner(
+        "qr-reader", { fps: 30, qrbox: 250 });
+    html5QrcodeScanner.render(onScanSuccess);
+
+    function createButtonLink(decodedText){
+        window.location.replace(decodedText);
+    }
+
 </script>
-</body>
-</html>
