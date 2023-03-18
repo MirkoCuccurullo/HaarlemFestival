6e5b7a145493fe5ab7507a38c343539e4392dff5
Mirko Cuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2023-03-18 19:11:22 +0100

    trial of webcam on web browser

--- app/view/festival/qr_checker.php
+++ app/view/festival/qr_checker.php
@@ -1,21 +1,62 @@
-<!doctype html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Open webcam using JavaScript.</title>
+    }
+    #videoCam {
+    width: 630px;
+    height: 300px;
+    margin-left: 0px;
+    <style>
+        *{
+            background-color: #658EA9;
+            border: 3px solid #ccc;
+            background: black;
+        }
+        #startBtn {
+            margin-left: 280px;
+            width: 120px;
+            height: 45px;
+            cursor: pointer;
+            font-weight: bold;
+        }
+        #startBtn:hover{
+            background-color: #647C90;
+            color: red;
+        }
+    </style>
 </head>
 <body>
-
-</body>
+<h1>Open WebCam Using JavaScript</h1>
+<br/>
+<video id="videoCam"></video>
+<br/><br/>
+<button id="startBtn" onclick="openCam()">Open Camera</button>
 <script>
-    if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-        console.log("Let's get this party started")
+    function openCam(){
+        let All_mediaDevices=navigator.mediaDevices
+        if (!All_mediaDevices || !All_mediaDevices.getUserMedia) {
+            console.log("getUserMedia() not supported.");
+            return;
+        }
+        All_mediaDevices.getUserMedia({
+            audio: true,
+            video: true
+        })
+            .then(function(vidStream) {
+                var video = document.getElementById('videoCam');
+                if ("srcObject" in video) {
+                    video.srcObject = vidStream;
+                } else {
+                    video.src = window.URL.createObjectURL(vidStream);
+                }
+                video.onloadedmetadata = function(e) {
+                    video.play();
+                };
+            })
+            .catch(function(e) {
+                console.log(e.name + ": " + e.message);
+            });
     }
-
-    navigator.mediaDevices.getUserMedia({video: true});
-    
 </script>
+</body>
 </html>
