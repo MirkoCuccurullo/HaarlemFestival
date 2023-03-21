489d1b9e9bfe0e72e7f92dc4d33740dd8ec0407d
MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2023-03-21 09:44:30 +0100

    qr code generator implemented

---
+++ app/service/qrCodeGenerator.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\Label\Label;
+use Endroid\QrCode\Logo\Logo;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+use Endroid\QrCode\Writer\PngWriter;
+
+class qrCodeGenerator
+{
+
+    public function generateQrCode($link){
+
+        $writer = new PngWriter();
+
+        $url = $link;
+
+        // Create QR code
+        $qrCode = QrCode::create($url)
+            ->setEncoding(new Encoding('UTF-8'))
+            ->setErrorCorrectionLevel(new ErrorCorrectionLevelLow())
+            ->setSize(300)
+            ->setMargin(10)
+            ->setRoundBlockSizeMode(new RoundBlockSizeModeMargin())
+            ->setForegroundColor(new Color(0, 0, 0))
+            ->setBackgroundColor(new Color(255, 255, 255));
+
+        // Create generic logo
+        $logo = Logo::create(__DIR__.'/assets/symfony.png')
+            ->setResizeToWidth(50);
+
+        // Create generic label
+        $label = Label::create('Label')
+            ->setTextColor(new Color(255, 0, 0));
+
+        $result = $writer->write($qrCode, $logo, $label);
+
+        // Validate the result
+        //$writer->validateResult($result, 'Life is too short to be generating QR codes');
+        return $result->getDataUri();
+    }
+}
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '61a6a7c148a88e1c1a221053a3cba527bf41a30b',
+        'reference' => '5f365bcf3b0681819a0f89c7b7151bf82b4921b4',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '61a6a7c148a88e1c1a221053a3cba527bf41a30b',
+            'reference' => '5f365bcf3b0681819a0f89c7b7151bf82b4921b4',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
