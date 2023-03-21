fb88a4aefe8be0c1dc6ced46e4d52c13e413d9ca
RobyParak <677676@student.inholland.nl>
2023-03-21 09:38:13 +0100

    Api restaurant working again

--- app/view/yummy/yummy_homepage.php
+++ app/view/yummy/yummy_homepage.php
@@ -10,24 +10,24 @@
         <div class="restaurant-card">
             <!-- Restaurant name and photo -->
             <div class="restaurant-name">
-                <h3><?= $restaurant['name'] ?></h3>
-                <img src="<?= $restaurant['photo'] ?>" alt="<?= $restaurant['name'] ?>">
+                <h3><?= $restaurant->name ?></h3>
+                <img src="<?= $restaurant->photo ?>" alt="<?= $restaurant->name ?>">
             </div>
 
             <!-- Restaurant description -->
             <div class="restaurant-description">
-                <p><?= $restaurant['description'] ?></p>
+                <p><?= $restaurant->description ?></p>
             </div>
 
             <!-- Cuisines and dietary information -->
             <div class="restaurant-info">
                 <div class="restaurant-cuisines">
                     <h4>Cuisines:</h4>
-                    <p><?= $restaurant['cuisines'] ?></p>
+                    <p><?= $restaurant->cuisines ?></p>
                 </div>
                 <div class="restaurant-dietary-info">
                     <h4>Dietary Information:</h4>
-                    <p><?= $restaurant['dietary'] ?></p>
+                    <p><?= $restaurant->dietary?></p>
                 </div>
             </div>
 
@@ -35,8 +35,8 @@
             <div class="restaurant-sessions">
                 <h4>Session Times:</h4>
                 <ul>
-                    <?php foreach ($restaurant['sessions'] as $session): ?>
-                        <li><?= $session['startTime'] ?> - <?= $session['endTime'] ?></li>
+                    <?php foreach ($restaurant->sessions as $session): ?>
+                        <li><?= $session->startTime ?> - <?= $session->endTime ?></li>
                     <?php endforeach; ?>
                 </ul>
                 <!-- add a button here to reserve -->
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '5765fff9aef1805016a0731a1073bcdb0a9ed409',
+        'reference' => 'a1079cdfe01a587c0f0887ad3d43d5374b68c91b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '5765fff9aef1805016a0731a1073bcdb0a9ed409',
+            'reference' => 'a1079cdfe01a587c0f0887ad3d43d5374b68c91b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
